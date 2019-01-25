c7559c5948fe364ef06b1296c6d57c694d7e7f50
Pavel Zanchuk <buzzik@yahoo.com>
2019-01-25 10:36:30 -1000

    add styling to index.html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,26 @@
-// function translator(string){
-//   var findVowel = string.match(/[aeiou]/gi);
-//   // alert(vowel.slice(0,1)); //shows firs vowel in the word
-//   var firstVowel = findVowel.slice(0,1); //we need this idk why
-//   var vowel = string.indexOf(firstVowel); // returns -1 = never occurs, .search() - does same thing
-//   if(vowel === 0){
-//     return string.slice(1) + string.charAt(0) + "way";
-//   }else if(string.charAt(0)==="q" && string.charAt(1)==="u"){
-//     return string.slice(2) + string.charAt(0) + string.charAt(1) + "ay";
-//   } else {
-//     return string.slice(vowel) + string.slice(0,vowel) + "ay";
-//   }
-//
-// }
 function replacer(number){
-  if(number % 3 === 0){
-    alert("I'm sorry, Dave. I'm afraid I can't do that")
-  }else if(number.toString().includes("1")){
-    alert("Boop")
-  }else if(number.toString().includes("0")){
-    alert("Beep");
+  if(number){
+    if(number % 3 === 0){
+      return "I'm sorry, Dave. I'm afraid I can't do that";
+    }else if(number.toString().includes("1")){
+      return "Boop!";
+    }else if(number.toString().includes("0")){
+      return "Beep!";
+    }else{
+      return number;
+    }
   }else{
-    alert(number);
+    alert("Please enter a numeric value")
   }
 }
-
-
 
 
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#number").val());
-    // var inputArray = userInput.split(" ");
-    replacer(userInput);
+    $("#result").append(replacer(userInput)+ ", ");
+    ;
 
   });
  });
