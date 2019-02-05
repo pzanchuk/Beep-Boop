dab7d8877e113c79ac8a7d98305a909e259034ef
Pavel Zanchuk <buzzik@yahoo.com>
2019-02-05 00:50:34 -0800

    Apdate business and user logic code to meet project's criteria, update README.md file to reflect all the changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
 function replacer(number, name){
-  if(number || number === 0){
-    if(number % 3 === 0){
-      return "\"I'm sorry, "+name+". I'm afraid I can't do that.\"";
-    }else if(number.toString().includes("1")){
-      return "\"Boop!\"";
-    }else if(number.toString().includes("0")){
+  if(number % 3 === 0){
+    if(number === 0){
       return "\"Beep!\"";
     }else{
-      var numberToArray = number.toString().split("");
-      var reversed = "";
-      for( var i = numberToArray.length-1; i >= 0; i --){
-        reversed += numberToArray[i];
-      } return reversed;
+      return "\"I'm sorry, "+name+". I'm afraid I can't do that.\"";
     }
+  }else if(number.toString().includes("1")){
+    return "\"Boop!\"";
+  }else if(number.toString().includes("0")){
+    return "\"Beep!\"";
   }else{
-    alert("Please enter a numeric value!");
-    return "I am not a number";
+    return number;
   }
+
 }
 
 $(document).ready(function() {
-
   $("form#form").submit(function(event) {
     event.preventDefault();
     var userName = $("input#name").val();
     var userInput = parseInt($("input#number").val());
-    $("#result").append(replacer(userInput, userName)+ " ");
+    if(!isNaN(userInput)){
+      for(var i = 0; i <= userInput; i++){
+        $("#result").append(replacer(i, userName)+ " ");
+      }
+    }else{
+      alert("NOT a number")
+    }
     $(".output").show();
   });
  });
